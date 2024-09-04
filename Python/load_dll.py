277d85ddfbefe8e21fcbfaeb76e6dadd1d7b7978
Polozovs <viktors.polozovs@accenture.com>
2024-09-04 23:13:30 +0300

    commit

--- Python/load_dll.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Imports IBM DB2 dll
-def load_dll():
-    current_directory = os.getcwd()
-    os.add_dll_directory(f"{current_directory}\\.venv\\Lib\\site-packages\\clidriver\\bin")
-    
-    import ibm_db
